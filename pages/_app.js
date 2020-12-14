2afce3840cbdf1b4f22974971fb9f489f11b64a5
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-14 22:45:11 +0700

    Menambahkan theme dan menghapus styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import useDarkMode from "use-dark-mode";
+import { lightTheme, darkTheme } from "../config/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const darkMode = useDarkMode(true);
+  const theme = darkMode ? darkTheme : lightTheme;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
