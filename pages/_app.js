230b5b82b0737235b56ef6db2edffdd01d3326a0
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-15 19:44:39 +0700

    Menambahkan open graph

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { GlobalStyles } from "../config/GlobalStyles";
 import { lightTheme, darkTheme } from "../config/theme";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
+import { NextSeo } from "next-seo";
 
 const Navigasi = dynamic(() => import("../components/Navigasi"), {
   loading: () => (
@@ -41,6 +42,29 @@ function MyApp({ Component, pageProps }) {
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content={darkMode ? "#323234" : "#f0efeb"} />
       </Head>
+      <NextSeo
+        openGraph={{
+          url: process.env.URL,
+          title: "Karya Ilmiah Remaja SMP Negeri 13",
+          description:
+            "Website resmi Karya Ilmiah Remaja SMPN 13 Bekasi. Karya Ilmiah Remaja ini adalah ekskul yang bertemakan tentang Sains dan Ilmu Pengetahuan Umum",
+          images: [
+            {
+              url: process.env.URL + "/op-big.png",
+              width: 1200,
+              height: 630,
+              alt: "KIR Open Graph Beeg",
+            },
+            {
+              url: process.env.URL + "/op-small.png",
+              width: 484,
+              height: 484,
+              alt: "KIR Open Graph Smol",
+            },
+          ],
+          site_name: "Sekilas 13",
+        }}
+      />
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Navigasi darkMode={darkMode} />
