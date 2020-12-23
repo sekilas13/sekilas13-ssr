6d90249c984c77379bbe0adfb6802ecb39b1cc0b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 16:59:14 +0700

    Menggunakan css module untuk jumbotron

--- components/main/JumbotronTop.js
+++ components/main/JumbotronTop.js
@@ -1,11 +1,15 @@
 import { Jumbotron, Container, Row } from "react-bootstrap";
+import styles from "../../styles/Jumbotron.module.css";
 
 export default function JumbotronTop() {
   return (
-    <Jumbotron fluid>
-      <Container>
+    <Jumbotron fluid className={styles.jumbotron}>
+      <Container className={styles.container}>
         <Row className="justify-content-center">
-          <h1 id="tsukilas">Sekilas !</h1>
+          <h1 className={styles.hasatu}>Sekilas !</h1>
+        </Row>
+        <Row className="justify-content-center">
+          <p className={styles.paragrap}>Semangat KIR Tiga Belas !</p>
         </Row>
       </Container>
     </Jumbotron>
