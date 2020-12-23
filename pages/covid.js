a11f948118620fc6c696844100eaced22bbc59a8
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 22:52:12 +0700

    Menambahkan halaman baru untuk info covid

---
+++ pages/covid.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Covid() {
+  return (
+    <Head>
+      <title>Sekilas 13 | Informasi Covid 19</title>
+      <meta
+        name="description"
+        content="Informasi penyebaran virus corona di Indonesia dengan tampilan web dari Karya Ilmiah Remaja SMPN 13 Bekasi"
+      />
+    </Head>
+  );
+}
