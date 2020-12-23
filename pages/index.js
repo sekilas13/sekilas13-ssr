65456cfcd50f5dd4a650ad379ab82602160c5ae3
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 12:51:16 +0700

    Menambahkan meta description

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <Head>
       <title>Karya Ilmiah Remaja SMP Negeri 13 Bekasi</title>
+      <meta
+        name="description"
+        content="Website resmi Karya Ilmiah Remaja SMPN 13 Bekasi. Karya Ilmiah Remaja ini adalah ekskul yang bertemakan tentang Sains dan Ilmu Pengetahuan Umum"
+      />
     </Head>
   );
 }
