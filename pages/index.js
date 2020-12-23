8b7eaa21fbb59854c950e2587709dfd14405565f
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 16:31:43 +0700

    Mengimport komponen main

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
+import Content from "../components/main";
 
 export default function Home() {
   return (
-    <Head>
-      <title>Karya Ilmiah Remaja SMP Negeri 13 Bekasi</title>
-      <meta
-        name="description"
-        content="Website resmi Karya Ilmiah Remaja SMPN 13 Bekasi. Karya Ilmiah Remaja ini adalah ekskul yang bertemakan tentang Sains dan Ilmu Pengetahuan Umum"
-      />
-    </Head>
+    <>
+      <Head>
+        <title>Karya Ilmiah Remaja SMP Negeri 13 Bekasi</title>
+        <meta
+          name="description"
+          content="Website resmi Karya Ilmiah Remaja SMPN 13 Bekasi. Karya Ilmiah Remaja ini adalah ekskul yang bertemakan tentang Sains dan Ilmu Pengetahuan Umum"
+        />
+      </Head>
+      <Content />
+    </>
   );
 }
