25f09e9a9c4ca489c39208247ed6c758d3cf0dfe
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 17:35:17 +0700

    Menambahkan file deskripsi

--- components/main/index.js
+++ components/main/index.js
@@ -1,5 +1,11 @@
 import JumbotronTop from "./JumbotronTop";
+import Deskripsi from "./Deskripsi";
 
 export default function Main() {
-  return <JumbotronTop />;
+  return (
+    <>
+      <JumbotronTop />
+      <Deskripsi />
+    </>
+  );
 }
