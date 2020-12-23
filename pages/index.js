203ed66ebb8d7aa0e0f707487dc2c82bcddd94ad
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 20:05:26 +0700

    Menambahkan google font dan link rel preconnect

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,25 @@ export default function Home() {
           name="description"
           content="Website resmi Karya Ilmiah Remaja SMPN 13 Bekasi. Karya Ilmiah Remaja ini adalah ekskul yang bertemakan tentang Sains dan Ilmu Pengetahuan Umum"
         />
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Eczar:wght@600&family=Roboto&family=Kufam&display=swap"
+          as="style"
+          onload="this.onload=null;this.rel='stylesheet'"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Eczar:wght@600&family=Roboto&family=Kufam&display=swap"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Eczar:wght@600&family=Roboto&family=Kufam&display=swap"
+          />
+        </noscript>
+
+        <link rel="preconnect" href="https://via.placeholder.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
       <Content />
     </>
