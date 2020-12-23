e9134eee615756846ef4f7efb0752ce5d1d51d06
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 17:26:05 +0700

    Menambahkan bagian gambar

--- components/main/JumbotronTop.js
+++ components/main/JumbotronTop.js
@@ -6,6 +6,13 @@ export default function JumbotronTop() {
     <Jumbotron fluid className={styles.jumbotron}>
       <Container className={styles.container}>
         <Row className="justify-content-center">
+          <img
+            src="/assets/real/KIR.png"
+            className={`${styles.gambar} img-fluid text-center rounded`}
+            alt="Logo KIR"
+          />
+        </Row>
+        <Row className="justify-content-center">
           <h1 className={styles.hasatu}>Sekilas !</h1>
         </Row>
         <Row className="justify-content-center">
