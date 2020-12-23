823c9152a465202ec361d1fca39de0d936d922ee
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 12:52:36 +0700

    Menambahkan bootstrap css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Navigasi from "../components/Navigasi";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -6,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
+      <Navigasi />
       <Component {...pageProps} />
     </>
   );
