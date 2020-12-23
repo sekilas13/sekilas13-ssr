e7cbad4a0e8654ff885b5351bbda5ea938cf6af4
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 23:11:43 +0700

    Memanggil komponen covid

--- pages/covid.js
+++ pages/covid.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
+import Content from "../components/covid";
 
 export default function Covid() {
   return (
-    <Head>
-      <title>Sekilas 13 | Informasi Covid 19</title>
-      <meta
-        name="description"
-        content="Informasi penyebaran virus corona di Indonesia dengan tampilan web dari Karya Ilmiah Remaja SMPN 13 Bekasi"
-      />
-    </Head>
+    <>
+      <Head>
+        <title>Sekilas 13 | Informasi Covid 19</title>
+        <meta
+          name="description"
+          content="Informasi penyebaran virus corona di Indonesia dengan tampilan web dari Karya Ilmiah Remaja SMPN 13 Bekasi"
+        />
+      </Head>
+      <Content />
+    </>
   );
 }
