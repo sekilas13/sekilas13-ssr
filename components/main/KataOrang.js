8c27526fd3a5e6fc44726176611e1e29a136def2
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-23 20:00:41 +0700

    Menambahkan Carousel

--- components/main/KataOrang.js
+++ components/main/KataOrang.js
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import styles from "../../styles/KataOrang.module.css";
+import data from "../../assets/data/KataOrang";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function KataOrang() {
   const ref = useRef();
@@ -7,6 +9,9 @@ export default function KataOrang() {
     width: window.innerWidth,
     height: window.innerHeight,
   });
+  const [index, setIndex] = useState(3);
+
+  const handleSelect = (selectedIndex) => void setIndex(selectedIndex);
 
   useEffect(() => {
     const curr = ref.current;
@@ -25,5 +30,25 @@ export default function KataOrang() {
     };
   }, []);
 
-  return <section id={styles.KataOrang} ref={ref}></section>;
+  return (
+    <section id={styles.KataOrang} ref={ref}>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        {data.map((key, i) => (
+          <Carousel.Item key={i}>
+            <img
+              className="d-block w-100"
+              src={`https://via.placeholder.com/${ukuran.width}x${ukuran.height}/373940/fff&text=+`}
+              alt={key.alt}
+              width={ukuran.width}
+              height={ukuran.height}
+            />
+            <Carousel.Caption>
+              <h3>{key.capt[0]}</h3>
+              <p>{key.capt[1]}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </section>
+  );
 }
