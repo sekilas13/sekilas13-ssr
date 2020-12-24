efacd1601bac38c821d788293b1990cda84780cd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-24 16:15:14 +0700

    Menhapus next link

--- components/Navigasi/memoized/NavLink.js
+++ components/Navigasi/memoized/NavLink.js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import Nav from "react-bootstrap/Nav";
 import { memo } from "react";
-import Link from "next/link";
 
 const List = {
   "/": [
@@ -23,17 +22,15 @@ function NavLink() {
   };
 
   const renderer = List[pathname];
-  const to = pathname === "/" ? "/covid" : "/"
+  const to = pathname === "/" ? "/covid" : "/";
 
   return (
     <>
       {renderer && (
         <>
-        <Link href={to}>
           <Nav.Link href={to}>
             {pathname === "/" ? "Informasi Covid 19" : "Halaman Utama"}
           </Nav.Link>
-        </Link>
           {renderer.map((link, i) => (
             <Nav.Link key={i} href={link.to} id={link.to} onClick={handleLink}>
               {link.nama}
