366d179f0d6b43c9719d8d4394a4ecdbe27fb36c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-24 11:48:11 +0700

    Memanggil css card

--- components/covid/Custom/CardWrapper.js
+++ components/covid/Custom/CardWrapper.js
@@ -1,13 +1,14 @@
 import { Card, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "../../../styles/covid/Card.module.css";
 
 export default function CardWrapper({ label, data, icon }) {
   return (
-    <Card>
+    <Card className={styles.card}>
       <Card.Body>
         <Row className="justify-content-center h-100 d-flex">
           <Col md={4} className="text-center align-self-center">
-            <FontAwesomeIcon icon={icon} size="6x" />
+            <FontAwesomeIcon icon={icon} size="6x" className={styles.svg} />
           </Col>
           <Col md={8} className="text-center align-self-center">
             <h4>{label}</h4>
