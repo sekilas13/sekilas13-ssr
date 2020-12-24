29c38aae2a955f9e56682d749c4dd2859a057ed0
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-24 17:59:28 +0700

    Menggunakan react-progressive-image

--- components/main/JumbotronTop.js
+++ components/main/JumbotronTop.js
@@ -1,3 +1,4 @@
+import ProgressiveImage from "react-progressive-image";
 import { Jumbotron, Container, Row } from "react-bootstrap";
 import styles from "../../styles/main/Jumbotron.module.css";
 
@@ -6,11 +7,22 @@ export default function JumbotronTop() {
     <Jumbotron fluid className={styles.jumbotron}>
       <Container className={styles.container}>
         <Row className="justify-content-center">
-          <img
+          <ProgressiveImage
             src="/assets/real/KIR.png"
-            className={`${styles.gambar} img-fluid text-center rounded`}
-            alt="Logo KIR"
-          />
+            placeholder="/assets/real/KIR.placeholder.png"
+          >
+            {(src, loading) => (
+              <img
+                src={src}
+                style={{
+                  filter: loading ? "blur(3.5px)" : "none",
+                  transition: "all 0.40s linear",
+                }}
+                className={`${styles.gambar} img-fluid text-center rounded`}
+                alt="Logo KIR"
+              />
+            )}
+          </ProgressiveImage>
         </Row>
         <Row className="justify-content-center">
           <h1 className={styles.hasatu}>Sekilas !</h1>
