4d4f874d3ffb50cdd9576b94bd5cec2cfc972ebe
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-24 11:00:10 +0700

    Menambahkan custom component

---
+++ components/covid/Custom/CardWrapper.js
@@ -0,0 +1,20 @@
+import { Card, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function CardWrapper({ label, data, icon }) {
+  return (
+    <Card>
+      <Card.Body>
+        <Row className="justify-content-center h-100 d-flex">
+          <Col md={4} className="text-center align-self-center">
+            <FontAwesomeIcon icon={icon} size="6x" />
+          </Col>
+          <Col md={8} className="text-center align-self-center">
+            <h4>{label}</h4>
+            <h3>{data} Orang</h3>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+}
