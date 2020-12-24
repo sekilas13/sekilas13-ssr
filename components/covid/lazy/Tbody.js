09c8b4c3c7e64247d8dc058e83a65647b0f9d367
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-24 18:55:07 +0700

    Menampilkan data hasil fetch

--- components/covid/lazy/Tbody.js
+++ components/covid/lazy/Tbody.js
@@ -13,5 +13,40 @@ export default function Tbody() {
       .then((result) => setDataProvinsi(result));
   }, []);
 
-  return <></>;
+  return (
+    <>
+      {!data && (
+        <>
+          <tr>
+            <td>1</td>
+            <td colSpan={4} className="text-center">
+              Sedang mengambil data....
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td colSpan={4} className="text-center">
+              Mohon tunggu....
+            </td>
+          </tr>
+        </>
+      )}
+      {data && (
+        <>
+          {data
+            .filter((d) => d.provinsi !== "Indonesia")
+            .filter((d) => d.provinsi !== null)
+            .map((d, i) => (
+              <tr key={d.kodeProvi}>
+                <td>{i + 1}</td>
+                <td>{d.provinsi}</td>
+                <td>{Number(d.kasusPosi).toLocaleString()}</td>
+                <td>{Number(d.kasusSemb).toLocaleString()}</td>
+                <td>{Number(d.kasusMeni).toLocaleString()}</td>
+              </tr>
+            ))}
+        </>
+      )}
+    </>
+  );
 }
