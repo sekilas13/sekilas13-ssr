18480ff0ad878a13f28916eb198ffdd715ae4c01
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-24 19:46:07 +0700

    Menggunakan react-progressive-image di lazy component gambar

--- components/main/lazy/Gambar.js
+++ components/main/lazy/Gambar.js
@@ -1,6 +1,7 @@
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 import { Row, Container, Col, Image } from "react-bootstrap";
 import styles from "../../../styles/main/Gambar.module.css";
+import ProgressiveImage from "react-progressive-image";
 import gambar from "../../../assets/data/Gambar";
 
 export default function Gambar() {
@@ -11,11 +12,20 @@ export default function Gambar() {
           <Row>
             {gambar.map((g, i) => (
               <Col md={4} key={i}>
-                <Image
-                  src={g.src}
-                  alt={g.alt}
-                  className={`${styles.gambar} img-fluid img-thumbnail mt-3`}
-                />
+                <ProgressiveImage src={g.src} placeholder={g.placeholder}>
+                  {(src, loading) => (
+                    <Image
+                      src={src}
+                      alt={g.alt}
+                      style={{
+                        width: "100%",
+                        filter: loading ? "blur(3.5px)" : "none",
+                        transition: "all 0.40s linear",
+                      }}
+                      className={`${styles.gambar} img-fluid img-thumbnail mt-3`}
+                    />
+                  )}
+                </ProgressiveImage>
               </Col>
             ))}
           </Row>
