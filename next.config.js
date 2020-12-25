8e7f895d3f4e91c17db040ec6fc97053023422bb
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-25 09:54:53 +0700

    Mengganti next-images dengan next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,2 +1,25 @@
-const withImages = require("next-images");
-module.exports = withImages();
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
+  inlineImageLimit: 8192,
+  imagesFolder: "images",
+  imagesName: "[name]-[hash].[ext]",
+  handleImages: ["jpeg", "png", "webp"],
+  removeOriginalExtension: false,
+  optimizeImages: true,
+  optimizeImagesInDev: false,
+  mozjpeg: {
+    quality: 80,
+  },
+  optipng: {
+    optimizationLevel: 3,
+  },
+  pngquant: false,
+  webp: {
+    preset: "default",
+    quality: 75,
+  },
+  responsive: {
+    adapter: require("responsive-loader/sharp"),
+  },
+});
