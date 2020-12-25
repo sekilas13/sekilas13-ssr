8f991b4de9b002b30ee521c4899e757b4f914b63
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-25 20:41:08 +0700

    Mengcompose seluruh next js plugin

--- next.config.js
+++ next.config.js
@@ -1,25 +1,44 @@
-const withOptimizedImages = require("next-optimized-images");
+const withPWA = require("next-pwa");
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withOptimizedImages({
-  inlineImageLimit: 8192,
-  imagesFolder: "images",
-  imagesName: "[name]-[hash].[ext]",
-  handleImages: ["jpeg", "png", "webp"],
-  removeOriginalExtension: false,
-  optimizeImages: true,
-  optimizeImagesInDev: false,
-  mozjpeg: {
-    quality: 80,
-  },
-  optipng: {
-    optimizationLevel: 3,
-  },
-  pngquant: false,
-  webp: {
-    preset: "default",
-    quality: 75,
-  },
-  responsive: {
-    adapter: require("responsive-loader/sharp"),
-  },
-});
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      inlineImageLimit: 8192,
+      imagesFolder: "images",
+      imagesName: "[name]-[hash].[ext]",
+      handleImages: ["jpeg", "png", "webp"],
+      removeOriginalExtension: false,
+      optimizeImages: true,
+      optimizeImagesInDev: false,
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 3,
+      },
+      pngquant: false,
+      webp: {
+        preset: "default",
+        quality: 75,
+      },
+      responsive: {
+        adapter: require("responsive-loader/sharp"),
+      },
+    },
+  ],
+  [
+    withPWA,
+    {
+      pwa: {
+        disable: process.env.NODE_ENV === "development",
+        register: true,
+        scope: "/",
+        sw: "service-worker.js",
+        dest: "public",
+      },
+    },
+  ],
+]);
