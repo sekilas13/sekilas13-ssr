be031516b2caf6de2b36b07ad15aec692279251c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-25 21:48:09 +0700

    Menambahkan tema

---
+++ assets/data/Theme.js
@@ -0,0 +1,48 @@
+export const lightTheme = {
+  jumbotron: {
+    bg: "#FFF",
+  },
+  deskripsi: {
+    bg: " #fdfffc",
+    h2: "#353535",
+    p: "#011627",
+  },
+  kataOrang: "#f1faee",
+  carousel: {
+    color: "#000",
+    indicator: "#242423",
+    arrow: "333533",
+  },
+  gambar: {
+    bg: "#f8f9fa",
+  },
+  cardCovid: {
+    bg: "#fff",
+    text: "#212529",
+  },
+};
+
+export const darkTheme = {
+  jumbotron: {
+    bg: "#242423",
+    color: "#fffcf2",
+  },
+  deskripsi: {
+    bg: "#2a2a2e",
+    h2: "#f8f9fa",
+    p: "#eaeaea",
+  },
+  kataOrang: "#373940",
+  carousel: {
+    color: "#fff",
+    indicator: "#fff",
+    arrow: "fff",
+  },
+  gambar: {
+    bg: "#2b2d42",
+  },
+  cardCovid: {
+    bg: "#212529",
+    text: "#fff",
+  },
+};
