9122f2441f2f7b6c76b18cf2dde034a0b7a7ec17
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-28 11:20:55 +0700

    Menambahkan link preload untuk css

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,13 @@ import useDarkMode from "use-dark-mode";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 
+// Hajat preload
+import Switcher from "../styles/navigasi/Switcher.module.css";
+import Navi from "../styles/navigasi/Navigasi.module.css";
+import KatOR from "../styles/main/KataOrang.module.css";
+import Gambar from "../styles/main/Gambar.module.css";
+import Footer from "../styles/main/Footer.module.css";
+
 const Navigasi = dynamic(() => import("../components/Navigasi"), {
   loading: () => (
     <nav
@@ -38,16 +45,16 @@ export default function Home() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Eczar:wght@600&family=Roboto&family=Kufam&display=swap"
         />
-        <noscript>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Eczar:wght@600&family=Roboto&family=Kufam&display=swap"
-          />
-        </noscript>
+
+        <link rel="preload" href={Navi} as="style" />
+        <link rel="preload" href={KatOR} as="style" />
+        <link rel="preload" href={Gambar} as="style" />
+        <link rel="preload" href={Footer} as="style" />
+        <link rel="preload" href={Switcher} as="style" />
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme} prefetch={false}>
         <GlobalStyles />
         <Navigasi dark={dark} />
         <Content theme={dark.value} />
