2e76a75f8805c892b527cc4cbac82a168ffab9a9
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-29 18:27:59 +0700

    Menambahkan google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../utils/gtag";
 
 export default class Root extends Document {
   static async getInitialProps(ctx) {
@@ -13,6 +14,23 @@ export default class Root extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/logo192-apple-touch.png" />
           <link rel="manifest" href="manifest.json" />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `
+            }}
+          />
         </Head>
         <body>
           <Main />
