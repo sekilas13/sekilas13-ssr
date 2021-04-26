38ddd21112e83ba2c55150f9756fd0ab0ae2e2ce
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-04-26 22:01:58 +0700

    Mengubah teknik import footer component

--- components/main/index.js
+++ components/main/index.js
@@ -1,11 +1,12 @@
 import { memo } from "react";
 import dynamic from "next/dynamic";
+
 import JumbotronTop from "./JumbotronTop";
 import Deskripsi from "./Deskripsi";
+import Footer from "./Footer";
 
 const KataOrang = dynamic(() => import("./KataOrang"), { ssr: false });
 const Gambar = dynamic(() => import("./Gambar"), { ssr: false });
-const Footer = dynamic(() => import("./Footer"), { ssr: false });
 
 const main = () => (
   <>
