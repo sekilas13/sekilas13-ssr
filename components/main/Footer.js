6ded58dc64e7dc51eec7c83e8eee4adc47a4728b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-04-26 22:00:01 +0700

    Menghapus loading dan menambahkan ssr false

--- components/main/Footer.js
+++ components/main/Footer.js
@@ -4,28 +4,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import styles from "../../styles/main/Footer.module.css";
 
 const Email = dynamic(() => import("./lazy/FooterEmail"), {
-  loading: () => (
-    <p>
-      Ada pertanyaan ? Tanyakan lewat email{" "}
-      <a href="mailto:smpn13kir@gmail.com">smpn13kir@gmail.com</a>
-    </p>
-  )
+  ssr: false
 });
 
 const Info = dynamic(() => import("./lazy/FooterInfo"), {
-  loading: () => (
-    <p>
-      Dibuat oleh{" "}
-      <a
-        href="https://github.com/sekilas13"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Karya Ilmiah Remaja
-      </a>{" "}
-      SMP Negeri 13 Bekasi | {new Date().getFullYear()}
-    </p>
-  )
+  ssr: false
 });
 
 export default function Footer() {
@@ -34,12 +17,12 @@ export default function Footer() {
       <Container>
         <Row className="pt-3 justify-content-center text-center">
           <Col md={4}>
-            <LazyLoad>
+            <LazyLoad once>
               <Email />
             </LazyLoad>
           </Col>
           <Col md={4}>
-            <LazyLoad>
+            <LazyLoad once>
               <Info />
             </LazyLoad>
           </Col>
