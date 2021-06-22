a341c5e3e18282b78d1b01cdf6eaf8a2736a2274
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-06-23 01:01:37 +0700

    Menghapus data gambar jumbotron

--- assets/data/GambarJumbotron.js
+++
@@ -1,7 +0,0 @@
-const source = require("../Img/KIR/KIR.png?resize&size=228&format=webp");
-const placeholder = require("../Img/KIR/KIR.png?resize&size=15&format=webp");
-
-module.exports = {
-  source,
-  placeholder
-};
