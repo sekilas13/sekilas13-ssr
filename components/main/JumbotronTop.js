e4ec4ec4d1ac590ac240339e7fadbc7871155919
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-06-24 17:29:19 +0700

    Menambahkan row reset dan banner

--- components/main/JumbotronTop.js
+++ components/main/JumbotronTop.js
@@ -2,16 +2,21 @@ import { Jumbotron, Container, Row, Col } from "react-bootstrap";
 import styles from "../../styles/main/Jumbotron.module.css";
 import { memo } from "react";
 
+const banner = require("../../assets/Img/KIR/banner.jpg");
+
 function JumbotronTop() {
   return (
     <Jumbotron fluid className={styles.jumbotron}>
-      <Row>
+      <Row className={styles.rowReset}>
         <Col className={styles.colText}>
           <h1 className={styles.welcome}>SELAMAT DATANG</h1>
           <h2 className={styles.diWebsite}>DI WEBSITE</h2>
           <h3 className={styles.eskul}>KARYA ILMIAH REMAJA</h3>
           <h3 className={styles.school}>SMPN 13 BEKASI</h3>
         </Col>
+      </Row>
+      <Row>
+        <img src={banner} className={styles.banner} />
       </Row>
     </Jumbotron>
   );
